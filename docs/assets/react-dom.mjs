b80ecc3d5f70ce843b1f2fe965fac5ca73249b00
lamartinecabral <lamartine.cb@gmail.com>
2026-08-08 20:05:36 -0300

    refactor: update CDN imports

--- docs/assets/react-dom.mjs
+++
@@ -1,9 +0,0 @@
-import "react";
-import "https://cdn.jsdelivr.net/npm/react-dom@18.3.1/umd/react-dom.development.js";
-
-/** @type {typeof import("react-dom/client")} */
-const ReactDOM = window.ReactDOM;
-
-export const createRoot = ReactDOM.createRoot;
-
-export default ReactDOM;
--- docs/assets/supabase.mjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('@supabase/supabase-js')} */
-const supabase = window.supabase;
-
-export const createClient = supabase.createClient;
-export default supabase;
